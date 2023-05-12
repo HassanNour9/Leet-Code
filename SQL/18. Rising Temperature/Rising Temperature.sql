28cac6ba9018b08e730009d6e16cb1a21faeda03
Hassan Nour <hassannour972000@gmail.com>
2023-05-12 18:59:41 +0300

    Update Rising Temperature.sql

--- SQL/18. Rising Temperature/Rising Temperature.sql
+++ SQL/18. Rising Temperature/Rising Temperature.sql
@@ -2,3 +2,10 @@ SELECT DISTINCT a.Id
 FROM Weather a,Weather b
 WHERE a.Temperature > b.Temperature
 AND DATEDIFF(a.Recorddate, b.Recorddate) = 1;
+***Another Solution***
+with cte as(
+select id,recordDate,temperature,lag(temperature) over (order by recordDate asc) as prev_temp,
+    lag(recordDate) over(order by recordDate asc) as prev_date
+from Weather)
+select id from cte
+where temperature > prev_temp and recordDate-prev_date=1
